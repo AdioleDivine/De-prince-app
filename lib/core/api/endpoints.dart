03651f95aa01a8d7cb091344fbd855720fdd928e
AdioleDivine <adioledivine27@Gmail.com>
2020-11-10 23:14:31 +0100

    Sign Out

--- lib/core/api/endpoints.dart
+++ lib/core/api/endpoints.dart
@@ -2,4 +2,8 @@ class Endpoints {
   //-------------STUDENT SIDE
   //auth endpoints
   static final String studentLogin = 'authenticate';
+
+  //-------------STUDENT SIDE
+  //auth endpoints
+  static final String staffLogin = 'authenticateStaff';
 }
